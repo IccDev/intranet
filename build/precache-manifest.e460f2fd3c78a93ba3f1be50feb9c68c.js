c431b9a3656e0fdc39064597a0e46526cfbd3a3e
djedou <djedouarnaud.aman@gmail.com>
2022-11-13 00:32:11 +0100

    add build folder for github page

---
+++ build/precache-manifest.e460f2fd3c78a93ba3f1be50feb9c68c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "02d0fde77253b71ba501",
+    "url": "/static/css/main.3d9584b2.chunk.css"
+  },
+  {
+    "revision": "02d0fde77253b71ba501",
+    "url": "/static/js/main.02d0fde7.chunk.js"
+  },
+  {
+    "revision": "8379b1f77d74ce14076c",
+    "url": "/static/js/1.8379b1f7.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5714f42cccf579668cf7de75880ad2be",
+    "url": "/static/media/logoicc2.5714f42c.png"
+  },
+  {
+    "revision": "ce3413cd4bcfde2ab6c0f08138be2a45",
+    "url": "/index.html"
+  }
+];
