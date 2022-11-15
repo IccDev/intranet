508c5563948d717fd5118d0e91b62c0644d91694
djedou <djedouarnaud.aman@gmail.com>
2022-11-15 21:30:59 +0100

    Updates

---
+++ precache-manifest.183ebf34752f6968ec2410315e0018e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca09d27ea71fad1a4307",
+    "url": "/intranet/static/css/main.c50d681e.chunk.css"
+  },
+  {
+    "revision": "ca09d27ea71fad1a4307",
+    "url": "/intranet/static/js/main.ca09d27e.chunk.js"
+  },
+  {
+    "revision": "8379b1f77d74ce14076c",
+    "url": "/intranet/static/js/1.8379b1f7.chunk.js"
+  },
+  {
+    "revision": "5cd198d04321fdc384a5",
+    "url": "/intranet/static/js/runtime~main.5cd198d0.js"
+  },
+  {
+    "revision": "5714f42cccf579668cf7de75880ad2be",
+    "url": "/intranet/static/media/logoicc2.5714f42c.png"
+  },
+  {
+    "revision": "5c2b75f21c4deb1695f00331c8e8f1a5",
+    "url": "/intranet/index.html"
+  }
+];
